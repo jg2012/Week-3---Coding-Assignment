94db277caa21fc87e3ba81e1cd874616a35024a1
jg764609 <jg764609@southeast.edu>
2022-07-15 17:55:40 -0500

    Updates to no. 5 & 6. + Descriptors added.

--- src/CodingAssignment.java
+++ src/CodingAssignment.java
@@ -1,45 +1,58 @@
+import java.util.Arrays;
 
 public class CodingAssignment {
 
 	public static void main(String[] args) {
-
+		//1A.) 
 		int [] ages = {3,9,23,64,2,8,28,93,25}; 
 		
+		//1B.) 
 		int x = ages[ages.length -1]; 
-		System.out.println("The first element subtracted from the last elemsent equals: " + (x-ages[0]));
-				
+		System.out.println("1B.) The first element subtracted from the last elemsent equals: " + (x-ages[0]));
+		
+		//1C.) 
 		double sum = 0; 
 		
 		for(int add: ages) {
 			sum+= add; 
 		}
 		double average = sum / ages.length; 
-		System.out.println("The average age is: " + average);
+		System.out.println("1C.) The average age is: " + average);
 		
-		String [] names = {"Sam","Tommy", "Tim","Sally","Buck", "Bob"}; 
+		//2A.) 
+		String [] names = {"2A.) Sam","Tommy", "Tim","Sally","Buck", "Bob"}; 
 		double sumOfLetters = 0; 
 		for(String name: names) {
 			sumOfLetters += name.length(); 
 		}
+		//2B.) 
 		double averageOfLetters = sumOfLetters / names.length; 
-		System.out.println("There is an average of " + averageOfLetters + " letters per name.");
-
-		System.out.println("The last element in the array is: " + ages[ages.length-1]);
-
-		System.out.println("The first Elment in array is: " + ages[0]);
+		System.out.println("2B.) There is an average of " + averageOfLetters + " letters per name.");
 		
+		//3.)
+		System.out.println("3.) The last element in the array is: " + ages[ages.length-1]);
 		
-		int [] nameLengths = new int [names.length]; 
+		//4.)
+		System.out.println("4.) The first Elment in array is: " + ages[0]);
+		
+		//5.)
+		int [] nameLengths = new int [6]; 
 	
 		for(int i = 0; i < names.length; i++) {
-			System.out.println(names[i]);	
+			nameLengths[i] = names[i].length(); 	
 		}
+		System.out.print("5.) Name Lengths for each element in the array are as follows: ");
+		System.out.println(Arrays.toString(nameLengths));
 		
+		//6.)
+		int namesum =0; 
+		for(int i = 0; i<nameLengths.length; i++) {
+			namesum = namesum + nameLengths[i]; 
+		}
+		System.out.print("6.) Sum of all letters equals: ");
+		System.out.println(namesum);
 		 
-		 int y=0; 
-		for(int sumOfElements = 0; sumOfElements < nameLengths.length; sumOfElements++) {
-			y = nameLengths[sumOfElements]; 
-		}System.out.println(y);
+		 
 		
 		
 		
